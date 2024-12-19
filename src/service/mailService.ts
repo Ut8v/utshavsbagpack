aa12eb6abebd8973c779f6694357b027fdbf684a
Utshav Khatiwada <utshavkhatiwada@MacBook-Pro.local>
2024-12-18 22:07:44 -0500

    service class to send mail

---
+++ src/service/mailService.ts
@@ -0,0 +1,18 @@
+import emailjs from 'emailjs-com';
+const serviceID = import.meta.env.VITE_SERVICE_ID;
+const templateID = import.meta.env.VITE_TEMPLATE_ID;
+const userID = import.meta.env.VITE_USER_ID;
+
+class mailService {
+    static async sendMail(formData: { name: string; email: string; message: string; }) {
+    const response = await emailjs.send(
+        serviceID, 
+        templateID, 
+        formData,
+        userID 
+      )
+        return response;
+    }
+}
+
+export default mailService;

f3f43694d8f6da5c2bb93790a54f5c9b57cb74aa
Utshav Khatiwada <utshavkhatiwada@MacBook-Pro.local>
2025-01-01 18:45:08 -0500

    added box shodow to the navbar

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -8,9 +8,9 @@ import ChatModal from './chat.tsx';
 
 const TopBar: React.FC = () => {
   return (
-    <Navbar expand="lg" className="navbar-custom fixed-top">
-      <Container>
-        <Navbar.Brand className="brand">
+    <Navbar expand="lg" className="navbar-custom fixed-top shadow-navbar">
+    <Container>
+      <Navbar.Brand className="brand">
         <Link to="/" className="brand-link">
           <img
             src={logo}
@@ -19,29 +19,29 @@ const TopBar: React.FC = () => {
             className="d-inline-block align-top"
           />
         </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <NavDropdown
-              title="Blogs"
-              id="basic-nav-dropdown"
-              className="nav-dropdown"
-            >
-              <NavDropdown.Item>
-                <Link to="/chat-blog" className="dropdown-link">
-                  Chat-Blog
-                </Link>
-              </NavDropdown.Item>
-            </NavDropdown>
-            <SocialIcons />
-          </Nav>
-          <div className="ms-auto d-flex align-items-center chat-model position-relative">
-            <ChatModal />
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <NavDropdown
+            title="Blogs"
+            id="basic-nav-dropdown"
+            className="nav-dropdown"
+          >
+            <NavDropdown.Item>
+              <Link to="/chat-blog" className="dropdown-link">
+                Chat-Blog
+              </Link>
+            </NavDropdown.Item>
+          </NavDropdown>
+          <SocialIcons />
+        </Nav>
+        <div className="ms-auto d-flex align-items-center chat-model position-relative">
+          <ChatModal />
+        </div>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>  
   );
 };
 
